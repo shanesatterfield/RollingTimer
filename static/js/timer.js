0d6f3391a9f8975684a3aa336df2b968144b617b
Shane Satterfield <dustyplant@gmail.com>
2015-12-08 00:00:04 -0800

    Added basic timer functionality.

--- static/js/timer.js
+++ static/js/timer.js
@@ -11,14 +11,17 @@ app.config(['$interpolateProvider', function($interpolateProvider) {
 app.controller('TimerListController', function($scope) {
 
     $scope.timers = [
-        { id: 0, time: '1 minute 30 seconds', valid: true },
-        { id: 1, time: '2 hr 45 min 10 sec',  valid: true },
-        { id: 2, time: '2 hours 10 minutes 30 seconds', valid: true }
+        { id: 1, time: '1 minute 30 seconds', valid: true },
+        { id: 2, time: '2 hr 45 min 10 sec',  valid: true },
+        { id: 3, time: '2 hours 10 minutes 30 seconds', valid: true }
     ];
 
     // $scope.timers = [];
+    $scope.currTime = { hr: 0, min: 0, sec: 0 };
+    $scope.endTime = -1;
+    $scope.tempTimer = { id: 0 };
 
-    $scope.tempTimer = {};
+    var timeInterval;
     var nextTimerID = 3;
     var timerRegex = new RegExp("^(([0-9]+) (hour|Hour|hr|h)s{0,1}){0,1}?\\s*(([0-9]+) (minute|Minute|min|m)s{0,1}){0,1}?\\s*(([0-9]+) (second|Second|sec|s)s{0,1}){0,1}?$");
 
@@ -27,8 +30,13 @@ app.controller('TimerListController', function($scope) {
         if($scope.tempTimer.valid) {
             $scope.tempTimer.id = nextTimerID++;
             $scope.timers.push($scope.tempTimer);
-            $scope.tempTimer = {}
+            $scope.tempTimer = {id: 0}
+
+            var timerGroup = $('#timerGroup' + $scope.tempTimer.id);
+            timerGroup.removeClass('has-success');
+            timerGroup.removeClass('has-error');
         }
+
     };
 
     $scope.checkValid = function(timer) {
@@ -57,6 +65,34 @@ app.controller('TimerListController', function($scope) {
 
     // TODO: Actually add this functionality.
     $scope.startTimers = function() {
+        var matches = timerRegex.exec($scope.timers[0].time)
+        $scope.currTime = {
+            hr:  parseInt(matches[2]) || 0,
+            min: parseInt(matches[5]) || 0,
+            sec: parseInt(matches[8]) || 0
+        };
+
+        $scope.endTime = new Date(new Date().getTime() + timeToMilli($scope.currTime.hr, $scope.currTime.min, $scope.currTime.sec));
+        $scope.currTime = getRemainingTime($scope.endTime);
+        timeInterval = setInterval(function() {
+            $scope.currTime = getRemainingTime($scope.endTime);
+            $scope.$apply();
+
+            if($scope.currTime.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }, 1000);
+    };
+
+    $scope.stopTimers = function() {
+
+    };
+
+    $scope.pause = function() {
+
+    };
+
+    $scope.unpause = function() {
 
     };
 });
@@ -87,3 +123,17 @@ app.animation('.repeated-anim', function() {
         }
     }
 });
+
+function timeToMilli(hours, minutes, seconds) {
+    return hours * 3600000 + minutes * 60000 +  seconds * 1000;
+}
+
+function getRemainingTime(end) {
+    var time = end - new Date();
+    return {
+        total: time,
+        hr:  Math.floor((time/(60 * 60 * 1000)) % 60),
+        min: Math.floor((time/(60*1000)) % 60),
+        sec: Math.floor((time/1000) % 60)
+    };
+}
