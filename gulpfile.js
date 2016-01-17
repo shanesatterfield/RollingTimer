10ef46c1e844506cdc85e110e21ab1acb092a4ef
Shane Satterfield <dustyplant@gmail.com>
2016-01-16 23:57:49 -0600

    Added basic compression with gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
 var Server = require('karma').Server;
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+gulp.task('compress', function() {
+    gulp.src(['static/js/helper.js', 'static/js/timer.js', 'static/js/anim.js'])
+        .pipe(concat('production.min.js'))
+        .pipe(gulp.dest('static/'));
+});
 
 gulp.task('test', function(done) {
     new Server({
