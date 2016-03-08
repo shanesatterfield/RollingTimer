f083ae2cf118edf29ac6355a80b79dd6644cd7d3
Shane Satterfield <dustyplant@gmail.com>
2016-03-07 23:52:15 -0600

    Updated build system.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
-var gulp = require('gulp');
+var gulp   = require('gulp');
 var Server = require('karma').Server;
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var clean  = require('gulp-clean');
 
 gulp.task('compress', function() {
     gulp.src(['static/js/helper.js', 'static/js/timer.js', 'static/js/anims.js'])
@@ -15,4 +16,9 @@ gulp.task('test', function(done) {
         configFile: __dirname + '/karma.conf.js',
         singleRun: true
     }, done).start();
+});
+
+gulp.task('clean', function() {
+    gulp.src(['node_modules/', 'static/bower_components'], {read: false})
+        .pipe(clean());
 });
